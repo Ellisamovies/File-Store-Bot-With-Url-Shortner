961b36d6d9b69caf05ef09ae9affc8809c6f1666
Ellisamovies <121487082+Ellisamovies@users.noreply.github.com>
2023-10-29 04:58:02 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -6,8 +6,8 @@ class Config(object):
   BOT_TOKEN = os.environ.get("BOT_TOKEN", "6757194553:AAHXj4bY4K9wkMstxl-mBM1VxnRzm_Qmm4w")
   BOT_USERNAME = os.environ.get("BOT_USERNAME", "Advancefilesbot")
   DB_CHANNEL = int(os.environ.get("DB_CHANNEL", "-1001994995496"))
-  SHORTLINK_URL = os.environ.get('SHORTLINK_URL', "MoneyKamalo.com")
-  SHORTLINK_API = os.environ.get('SHORTLINK_API', "0eefb93e1e3ce9470a7033115ceb1bad13a9d674")
+  SHORTLINK_URL = os.environ.get('SHORTLINK_URL', "onepagelink.in")
+  SHORTLINK_API = os.environ.get('SHORTLINK_API', "4443c1c22a92505c9770f1bbfdc33267a27cb370")
   BOT_OWNER = int(os.environ.get("BOT_OWNER", ""))
   DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://Nishantb:Nishantb@nishantb.k2uwtj0.mongodb.net/?retryWrites=true&w=majority")
   UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "")
@@ -32,11 +32,11 @@ Send Me any Media or File. I can Work In Channel too. Add Me to Channel with Edi
 ╰──────[ 😎 ]───────────⍟
 """
   ABOUT_DEV_TEXT = f"""
-🧑🏻‍💻 𝗗𝗲𝘃𝗲𝗹𝗼𝗽𝗲𝗿: [VJ](https://telegram.me/KingVj01)
+🧑🏻‍💻 𝗗𝗲𝘃𝗲𝗹𝗼𝗽𝗲𝗿: [VJ](https://telegram.me/ellisa_support)
  
  I am Super noob Please Support My Hard Work.
 
-[Donate Me](https://t.me/KingVj01)
+[Donate Me](https://t.me/ellisa_support)
 """
   HOME_TEXT = """
 Hello, [{}](tg://user?id={})\n\nThis is a Permanent **FileStore Bot**.
