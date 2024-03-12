7c19b5330e14efc95323c5bec31e90e935b9f6e6
Ellisamovies <121487082+Ellisamovies@users.noreply.github.com>
2024-03-13 01:03:15 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -11,7 +11,7 @@ class Config(object):
   BOT_OWNER = int(os.environ.get("BOT_OWNER", "1017302540"))
   DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://Nishantb:Nishantb@nishantb.k2uwtj0.mongodb.net/?retryWrites=true&w=majority")
   UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "-1001794551961")
-  LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1001794573996"))
+  LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1001853841702"))
   BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "").split())
   FORWARD_AS_COPY = bool(os.environ.get("FORWARD_AS_COPY", True))
   BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", True))
